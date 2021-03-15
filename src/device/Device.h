d439962517f8b783417bcd92253da7aa225d561b
Guilherme de Lemos <guilherme.eti@gmail.com>
2021-03-14 21:33:58 -0300

    feat(device): Create a method to return free SRAM.
    * feat(device): resolves #12
    
    * feat(device): Create a method to return free SRAM. Resolves open-aquarium/open-aquarium#12

---
+++ src/device/Device.h
@@ -0,0 +1,31 @@
+#ifndef OA_DEVICE_H
+#define OA_DEVICE_H
+
+#include <Arduino.h>
+
+/*
+ATmega328P
+Flash  32k bytes (of which .5k is used for the bootloader)
+SRAM   2k bytes
+EEPROM 1k byte
+
+ATmega2560
+Flash  256k bytes (of which 8k is used for the bootloader)
+SRAM   8k bytes
+EEPROM 4k byte
+*/
+class Device {
+
+    public:
+
+      // Device();
+
+      int getFreeSRAM();
+
+      // int getFreeFlash();
+      
+      // int getFreeEEPROM();
+
+};
+
+#endif
